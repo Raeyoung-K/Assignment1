809dbc1adfb3a1ace56627ad8c5c9217d77fe982
Raeyoung Kim <kimrae@sheridancollege.ca>
2022-09-27 00:12:44 -0400

    Initial commit- UI

---
+++ app/src/main/java/sheridan/kimrae/assignment1/PlayFragment.kt
@@ -0,0 +1,62 @@
+package sheridan.kimrae.assignment1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import sheridan.kimrae.assignment1.databinding.FragmentPlayBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [PlayFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class PlayFragment : Fragment() {
+
+    private var _binding: FragmentPlayBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentPlayBinding.inflate(inflater, container, false)
+
+        binding.playButton.setOnClickListener {
+            findNavController().navigate(R.id.action_playFragment_to_resultFragment)
+        }
+
+        return binding.root
+
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.playButton.setOnClickListener {
+            findNavController().navigate(R.id.action_playFragment_to_resultFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
